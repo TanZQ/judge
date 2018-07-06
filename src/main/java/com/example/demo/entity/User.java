67437f87eadce3ea0b4ceb8e548b7ff1ee212bd0
Your Name <you@example.com>
2018-07-06 16:52:54 +0800

    啦啦啦

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,16 @@
+package com.example.demo.entity;
+import lombok.Data;
+@Data
+public class User {
+        public String username;
+        public String password;
+        public String name;
+        public String sex;
+        public String dateofbirth;
+        public String address;
+        public String phoneno;
+        public String leader;
+        public String community;
+        public String company;
+        public String identity;
+}
