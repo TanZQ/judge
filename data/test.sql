aca4c5592e5a312aea278c846a71587e6158abf6
Kississ <40746927+Kississ@users.noreply.github.com>
2018-07-07 21:18:10 +0800

    Merge remote-tracking branch 'remotes/origin/Kississ'
    # Conflicts:
    #	data/test.sql

--- data/test.sql
+++ data/test.sql
@@ -20,41 +20,42 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 --  Table structure for `student`
 -- ----------------------------
-drop table if exists 'User';
-CREATE TABLE 'User'(
-  'username' varchar(8) character set utf8 default 'admin' comment '用户名',
-  'password' varchar(8) character set utf8 default '12345678' comment '密码',
-  'name' varchar(100) character set utf8 default '张三' comment '姓名',
-  'sex' varchar(8) CHARACTER SET utf8 DEFAULT '男' comment '性别',
-  'dateofbirth' varchar(8) character set utf8  default '20000101' comment '出生日期',
-  'address' varchar(100) character set utf8 default '空' comment '家庭住址',
-  'phoneno' varchar(11) default '17801127082' comment '联系方式',
-  'leader' varchar(100) character set utf8 default '空' comment '推荐人',
-  'community' varchar(100) default '行业分会1' comment '行业分会',
-  'company' varchar(100) default '专委会1' comment '专委会',
-  'identity' varchar(100) default 'waiting' comment '权限等级',
-  'useornot' varchar(100) default 'no' comment '使用与否',
-  primary key ('username')
+DROP TABLE IF EXISTS `User`;
+
+CREATE TABLE `User`(
+  `username` varchar(100) character set utf8 default 'admin' comment '用户名',
+  `password` varchar(100) character set utf8 default '12345678' comment '密码',
+  `name` varchar(100) character set utf8 default '张三' comment '姓名',
+  `sex` varchar(100) CHARACTER SET utf8 DEFAULT '男' comment '性别',
+  `dateofbirth` varchar(100) character set utf8  default '20000101' comment '出生日期',
+  `address` varchar(100) character set utf8 default '空' comment '家庭住址',
+  `phoneno` varchar(100) default '17801127082' comment '联系方式',
+  `leader` varchar(100) character set utf8 default '空' comment '推荐人',
+  `community` varchar(100) default '行业分会1' comment '行业分会',
+  `company` varchar(100) default '专委会1' comment '专委会',
+  `identity` varchar(100) default 'waiting' comment '权限等级',
+  `useornot` varchar(100) default 'no' comment '使用与否',
+  primary key (`username`)
 )engine =InnoDB DEFAULT CHARSET = utf8 comment='用户表';
 
 
-drop table if exists 'Correct';
-CREATE TABLE 'Correct'(
-  'Tno' int(8) default '001' comment '提案编号',
-  'Tname' varchar(8) character set utf8 default '空' comment '提案名称',
-  'author' varchar(100) character set utf8 default '张三' comment '提案人',
-  'date' varchar(8) CHARACTER SET utf8 DEFAULT '20000101' comment '日期',
-  'acceptornot' varchar(100) character set utf8 default 'no' comment '过审情况',
-  'acceptpeople' varchar(100) character set utf8 default '张三' comment '过审人',
-  'type' VARCHAR(100) character set utf8 default '提案' comment '类型',
-  'text' varchar(500) character set utf8 default '空' comment '文本',
-  primary key ('Tno')
+drop table if exists `Correct`;
+CREATE TABLE `Correct`(
+  `Tno` int(8) NOT NULL default 001 comment '提案编号',
+  `Tname` varchar(100) character set utf8 default '空' comment '提案名称',
+  `author` varchar(100) character set utf8 default '张三' comment '提案人',
+  `date` varchar(100) CHARACTER SET utf8 DEFAULT '20000101' comment '日期',
+  `acceptornot` varchar(100) character set utf8 default 'no' comment '过审情况',
+  `acceptpeople` varchar(100) character set utf8 default '张三' comment '过审人',
+  `type` VARCHAR(100) character set utf8 default '提案' comment '类型',
+  `text` varchar(500) character set utf8 default '空' comment '文本',
+  primary key (`Tno`)
 )engine =InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET = utf8 comment='规范表';
 -- ----------------------------
 --  Records of `student`
 -- ----------------------------
 BEGIN;
-INSERT INTO `User` VALUES ('admin', 'admin', '张三', '男','19980711','空','17801127082','空','空','空','manager','no');
+INSERT INTO `user` VALUES ('admin', 'admin', '张三', '男','19980711','空','17801127082','空','空','空','manager','no');
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
