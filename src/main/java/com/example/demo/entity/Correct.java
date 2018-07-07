8a6663c176cfcb01aff6b79c2a5e6978c63338f3
Your Name <you@example.com>
2018-07-07 15:58:58 +0800

    Merge remote-tracking branch 'remotes/origin/master' into tzq
    # Conflicts:
    #	logs/spring-boot-logging.log

---
+++ src/main/java/com/example/demo/entity/Correct.java
@@ -0,0 +1,13 @@
+package com.example.demo.entity;
+import lombok.Data;
+
+@Data
+public class Correct {
+   public  int Tno;
+   public String Tname;
+   public String author;
+   public String date;
+   public String acceptornot;
+   public String acceptpeople;
+   public String text;
+}
---
+++ src/main/java/com/example/demo/service/impl/Writer.java
@@ -0,0 +1,36 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.Correct;
+import com.example.demo.service.impl.CorrectServiceImpl;
+import com.example.demo.entity.User;
+import com.example.demo.service.impl.UserServiceImpl;
+import java.util.List;
+public class Writer {
+    public Correct correct;
+    public User user;
+    public List<Correct> correctlist;
+    public List<User> userlist;
+    public CorrectServiceImpl correctServiceImpl;
+    public UserServiceImpl userServiceImpl;
+    public List<Correct> proposalsearching(Correct correct){
+        this.correct=correct;
+        this.correctlist=correctServiceImpl.select(this.correct);
+        return this.correctlist;
+    }
+    public boolean proposalwriting(Correct correct){
+        this.correct=correct;
+        this.correct.acceptornot="no";
+        this.correct.acceptpeople="noone";
+        boolean success=correctServiceImpl.insert(this.correct);
+        return success;
+    }
+    public User informationmaintenance(){
+        this.user.useornot="yes";
+        this.userlist=userServiceImpl.select(this.user);
+        return this.userlist.get(0);
+    }
+    public boolean changeinformation(User user){
+        boolean success=userServiceImpl.update(user);
+        return success;
+    }
+}
