8c61d72e687a3f5912808b2dfe600ef331991691
Kississ <40746927+Kississ@users.noreply.github.com>
2018-07-09 00:07:29 +0800

    Merge remote-tracking branch 'remotes/origin/Kississ'
    # Conflicts:
    #	data/test.sql

--- src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -11,6 +11,7 @@ public class UserServiceImpl implements UserService{
 
     @Autowired
     private UserDao userDao;
+    private List<User> L1,L2;
 
     @Override
     public List<User> getAllUser()
@@ -21,7 +22,69 @@ public class UserServiceImpl implements UserService{
     @Override
     public List<User> select(User user)
     {
-        return userDao.select(user);
+        if(userDao.select(user.username)!=null)
+            L1=userDao.select(user.username);
+        if(userDao.select(user.password)!=null)
+        {
+            L2 = userDao.select(user.password);
+            L1.retainAll(L2);
+            L2=null;
+        }
+        if(userDao.select(user.name)!=null)
+        {
+            L2 = userDao.select(user.name);
+            L1.retainAll(L2);
+            L2=null;
+        }
+        if(userDao.select(user.sex)!=null)
+        {
+            L2 = userDao.select(user.sex);
+            L1.retainAll(L2);
+            L2=null;
+        }
+        if(userDao.select(user.dateofbirth)!=null)
+        {
+            L2 = userDao.select(user.dateofbirth);
+            L1.retainAll(L2);
+            L2=null;
+        }
+        if(userDao.select(user.phoneno)!=null)
+        {
+            L2 = userDao.select(user.phoneno);
+            L1.retainAll(L2);
+            L2=null;
+        }
+        if(userDao.select(user.leader)!=null)
+        {
+            L2 = userDao.select(user.leader);
+            L1.retainAll(L2);
+            L2=null;
+        }
+        if(userDao.select(user.community)!=null)
+        {
+            L2 = userDao.select(user.community);
+            L1.retainAll(L2);
+            L2=null;
+        }
+        if(userDao.select(user.company)!=null)
+        {
+            L2 = userDao.select(user.company);
+            L1.retainAll(L2);
+            L2=null;
+        }
+        if(userDao.select(user.identity)!=null)
+        {
+            L2 = userDao.select(user.identity);
+            L1.retainAll(L2);
+            L2=null;
+        }
+        if(userDao.select(user.useornot)!=null)
+        {
+            L2 = userDao.select(user.useornot);
+            L1.retainAll(L2);
+            L2=null;
+        }
+        return L1;
     }
 
     @Override
